5ac80c886f65b823f176dddb4ae85aa334729f8a
Logan Campbell <colin@logaan.net>
2017-06-06 17:25:31 +1000

    printing lines rather than showing list

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,9 +7,9 @@ import qualified Robot as R
 runCommandFile :: String -> IO ()
 runCommandFile path = do
   contents <- readFile path
-  putStrLn $ show (case (parseCommands contents) of
-                     Left _ -> ["Failed to parse."]
-                     Right result -> doRun result)
+  putStr $ (case (parseCommands contents) of
+               Left _ -> "Failed to parse.\n"
+               Right result -> unlines $ doRun result)
 
 doRun :: R.Command -> [String]
 doRun cs = readLogs (act run cs)
