a7a8c5d3b974f67e4c2c26e72c1d8532397f94a5
Logan Campbell <colin@logaan.net>
2017-06-06 17:46:02 +1000

    prevent fall off table

--- src/Point.hs
+++ src/Point.hs
@@ -2,3 +2,8 @@ module Point where
 
 data Point = Point Int Int
   deriving Show
+
+withinBounds :: Point -> Int -> Int -> Bool
+withinBounds (Point x y) width height =
+  0 <= x && x < width &&
+  0 <= y && y < height
--- src/Run.hs
+++ src/Run.hs
@@ -1,6 +1,8 @@
 module Run where
 
 import Robot
+import Point
+
 data Table = Table Int Int
 type Logs = [String]
 type Reason = String
@@ -12,12 +14,19 @@ data Run = NewRun Table
          | Run Table Robot Logs
 
 act :: Run -> Command -> Run
-act (NewRun table) (Place point direction) = Run table (Robot point direction) []
-act (NewRun table) (Commands commands) = foldl act (NewRun table) commands
-act (NewRun table) _ = (NewRun table)
+act newRun@(NewRun table@(Table width height)) (Place point direction) =
+  if withinBounds point width height
+  then Run table (Robot point direction) []
+  else newRun
+act newRun@(NewRun _) (Commands commands) = foldl act newRun commands
+act newRun@(NewRun _) _ = newRun
 
-act (Run table robot logs) (Place point direction) = Run table (place robot point direction) logs
-act (Run table robot logs) Move = Run table (move robot) logs
+act (Run table robot logs) (Place point direction) =
+  Run table newBot logs
+  where newBot = preventFall table robot (place robot point direction)
+act (Run table robot logs) Move =
+  Run table newBot logs
+  where newBot = preventFall table robot (move robot)
 act (Run table robot logs) Robot.Left = Run table (left robot) logs
 act (Run table robot logs) Robot.Right = Run table (right robot) logs
 act (Run table robot logs) Report = Run table robot (append (report robot) logs)
@@ -26,3 +35,9 @@ act run (Commands commands) = foldl act run commands
 readLogs :: Run -> [String]
 readLogs (NewRun _) = []
 readLogs (Run _ _ logs) = reverse logs
+
+preventFall :: Table -> Robot -> Robot -> Robot
+preventFall (Table width height) oldBot newBot@(Robot point _) =
+  if withinBounds point width height
+  then newBot
+  else oldBot
