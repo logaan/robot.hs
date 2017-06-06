073c1657fcf2bcbd1a9e154b1c149b3f205639fc
Logan Campbell <colin@logaan.net>
2017-06-06 23:05:17 +1000

    added bounds checking test cases

--- src/Examples.hs
+++ src/Examples.hs
@@ -12,7 +12,7 @@ a = doRun (Commands [Place (Point 0 0) North, Move, Report])
 
 b :: Bool
 b = doRun (Commands [Place (Point 0 0) North, Robot.Left, Report])
-    ["0,0,WEST"]
+          ["0,0,WEST"]
 
 c :: Bool
 c = doRun (Commands [Place (Point 1 2) East, Move, Move, Robot.Left, Move,
@@ -29,14 +29,34 @@ multiplePlacesAndReports =
         ["0,1,NORTH", "0,0,WEST", "3,3,NORTH"]
 
 noPlace :: Bool
-noPlace =
-  doRun (Commands [Move, Move, Report])
+noPlace = doRun
+  (Commands [Move, Move, Report])
   []
 
 latePlace :: Bool
 latePlace =
   doRun (Commands [Move, Move, Report, Place (Point 0 0) North, Move, Report])
-  ["0,1,NORTH"]
+        ["0,1,NORTH"]
+
+outOfBoundsPlace :: Bool
+outOfBoundsPlace = doRun
+  (Commands [Place (Point 5 5) North, Report])
+  []
+
+cornerPlace :: Bool
+cornerPlace = doRun
+  (Commands [Place (Point 4 4) North, Report])
+  ["4,4,NORTH"]
+
+outOfBoundsMoveNorth :: Bool
+outOfBoundsMoveNorth = doRun
+  (Commands [Place (Point 4 4) North, Move, Report])
+  ["4,4,NORTH"]
+
+outOfBoundsMoveSouth :: Bool
+outOfBoundsMoveSouth = doRun
+  (Commands [Place (Point 0 0) South, Move, Report])
+  ["0,0,SOUTH"]
 
 -- Runners
 run :: Run
@@ -46,4 +66,6 @@ doRun :: Command -> [String] -> Bool
 doRun cs output = readLogs (act run cs) == output 
 
 allPass :: Bool
-allPass = all id [a, b, c, multiplePlacesAndReports, noPlace, latePlace]
+allPass = all id [a, b, c, multiplePlacesAndReports, noPlace, latePlace,
+                  outOfBoundsPlace, cornerPlace, outOfBoundsMoveNorth,
+                  outOfBoundsMoveSouth]
